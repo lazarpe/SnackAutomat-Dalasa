f9783d0393c2250f05a1cb7eb49c690dc8aea7b1
davidemarcoli <davide.marcoli@noseryoung.ch>
2021-03-03 14:33:29 +0100

    edited Main.java

--- 20210303-SnackAutomat/src/ch/noseryoung/blj/Main.java
+++ 20210303-SnackAutomat/src/ch/noseryoung/blj/Main.java
@@ -1,16 +1,13 @@
 package ch.noseryoung.blj;
 
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("Fisch");
         int test = 4;
         System.out.println("Test var = " + test);
-
         test++;
-
         System.out.println(test);
 
-
+        
     }
 }
