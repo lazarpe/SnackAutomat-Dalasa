ac7d5546690c0aa895d8e2612c2c996574023c8a
davidemarcoli <davide.marcoli@noseryoung.ch>
2021-03-03 15:11:37 +0100

    added Classes
    Co-Authored-By: Davide Marcoli <69892203+davidemarcoli@users.noreply.github.com>

--- 20210303-SnackAutomat/src/ch/noseryoung/blj/Main.java
+++ 20210303-SnackAutomat/src/ch/noseryoung/blj/Main.java
@@ -8,6 +8,6 @@ public class Main {
         test++;
         System.out.println(test);
 
-        
+
     }
 }
---
+++ 20210303-SnackAutomat/src/ch/noseryoung/blj/Product.java
@@ -0,0 +1,15 @@
+package ch.noseryoung.blj;
+
+/****
+ ---------------------------------------------------------------------
+ Anwendung: Objektbasiert Programmieren NoserYoung
+ Author: Davide
+ Datum: 03.03.2021
+ Zeit: 15:10
+ Projekt: 20210303-SnackAutomat
+ Programm: Java Programm
+ Beschreibung:
+ ----------------------------------------------------------------------
+ ***/
+public class Product {
+}
