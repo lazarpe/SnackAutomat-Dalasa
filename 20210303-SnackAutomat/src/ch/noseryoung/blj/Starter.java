ed51ffc0141197ba49ab1cc34b3e83b8ce80475c
lazarpe <lazar.petrovic@noseryoung.ch>
2021-03-05 11:42:32 +0100

    printTitle method implemented with the vending machine name
    Co-Authored-By: lazarpe <69906272+lazarpe@users.noreply.github.com>

--- 20210303-SnackAutomat/src/ch/noseryoung/blj/Starter.java
+++ 20210303-SnackAutomat/src/ch/noseryoung/blj/Starter.java
@@ -2,6 +2,7 @@ package ch.noseryoung.blj;
 
 public class Starter {
     public static void main(String[] args) {
+        printTitle();
         VendingMachine vendingMachine = new VendingMachine(10, 5);
         vendingMachine.fillVendingMachine();
         vendingMachine.loadFromFile();
@@ -9,5 +10,14 @@ public class Starter {
         while (true) {
             vendingMachine.run();
         }
+    }
+
+    public static void printTitle() {
+        System.out.println("\n██████╗  █████╗   ██╗       █████╗  ███████╗ █████╗ \n" +
+                "██╔══██╗ ██╔══██╗ ██║      ██╔══██╗ ██╔════╝ ██╔══██╗\n" +
+                "██║  ██║ ███████║ ██║      ███████║ ███████╗ ███████║\n" +
+                "██║  ██║ ██╔══██║ ██║      ██╔══██║ ╚════██║ ██╔══██║\n" +
+                "██████╔╝ ██║  ██║ ███████╗ ██║  ██║ ███████║ ██║  ██║\n" +
+                "╚═════╝  ╚═╝  ╚═╝ ╚══════╝ ╚═╝  ╚═╝ ╚══════╝ ╚═╝  ╚═╝\n");
     }
 }
